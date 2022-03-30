427e24cfaf81c8124045c720a31f0dc3bceb279e
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-03-31 01:47:15 +1100

    Clarify deps

--- arcboot/src/drivers/mod.rs
+++ arcboot/src/drivers/mod.rs
@@ -1,4 +1,4 @@
-// FOR GENERIC DRIVERS
+// FOR GENERIC DRIVERS using UEFI tables/device trees
 // for ssds, timers, etc.
 // allows filesystem views
 
@@ -6,3 +6,43 @@
 
 struct TimerDriver;
 struct ModelDriver;
+
+// * use uefi logger
+
+use uefi::logger::Logger;
+use uefi::proto::console::text::{Output, OutputMode};
+
+trait New {
+    fn new() -> Self;
+}
+
+impl New for Output<'_> {
+    fn new() -> Self {
+        Self {
+            
+        }
+    }
+}
+
+// enable the logger when entering boot
+pub fn write() {
+    //     let output = Output{
+    //         reset: false,
+    //         output_string: "",
+    //         test_string: "",
+    // query_mode: (0,0,0),
+    // set_mode: (),
+    // set_attribute: (),
+    // clear_screen: false,
+    // set_cursor_position
+    // enable_cursor
+    // data
+    //     };
+
+    unsafe {
+        let mut output = Output::new();
+        let logger = Logger::new(&mut output);
+    }
+}
+
+// disable the logger as we are handing off to kernel
