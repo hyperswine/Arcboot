c4f8aceebfd125fdcae87979ac1a868e0a7a1ff6
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-06-21 18:35:56 +1000

    HEAP??

--- arcboot/src/memory/heap.rs
+++ arcboot/src/memory/heap.rs
@@ -25,8 +25,15 @@ pub fn zero_heap(heap_start: usize, heap_size: usize) {
 }
 
 // Temporary for Identity Mapping. 0xA000_0000 works too
+// Either: 0x4000_0000 which works
+// Doesnt work but kinda expected to work:
+// 0x0, 0x1000, 0x4000
+// but using them allows me to have MMIO memory descriptor...
+// STACK GROWS DOWN FROM a pretty high address (3.2G)
+// BUT FOR SOME REASON HEAP START ALWAYS AT 3.2G too?? Maybe it thinks it only needs 0x16_000 worth of memory so it puts it at 3.2G - 90K
+// Uh maybe make it 1GB as well. As the stack
 pub const HEAP_START: usize = 0x4000_0000;
-pub const HEAP_SIZE: usize = 4 * 0x1000;
+pub const HEAP_SIZE: usize = 0x4000_0000;
 
 // Can "reinit" the heap after 4 level page tables is setup with these virtual address ranges
 // For the kernel only. Userspace apps must also use virtual memory (lower half) instead
