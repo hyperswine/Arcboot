79de56de2833a757ed4da620647ad0c50391f017
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-07-10 21:35:28 +1000

    Updates!

--- export/src/lib.rs
+++ export/src/lib.rs
@@ -29,3 +29,28 @@ pub fn register_exception_handler(args: TokenStream, item: TokenStream) -> Token
     }
     .into()
 }
+
+pub enum DeviceType {
+    USBController,
+    PCIeController,
+    MainProcessor,
+    DRAM,
+    /// ROM, not mass storage (usb/pcie)
+    FlashMemory
+}
+pub struct PageTableTTBR1;
+
+pub struct ArcServices {
+    paging: PageTableTTBR1,
+    devices: &'static [ArcDevice],
+}
+
+pub struct ArcDevice {
+    device_type: DeviceType,
+    numa_id: usize,
+}
+
+#[test]
+fn test_basics() {
+    assert_eq!(1, 1)
+}
