8dc39fd4edd8c9c0156307958e057d6d70dd6d12
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-07-10 23:02:56 +1000

    Updates

--- export/src/lib.rs
+++ export/src/lib.rs
@@ -1,24 +1,77 @@
 #![no_std]
 
+// ---------------
+// ARCBOOT API
+// ---------------
+
+#[repr(C)]
 pub enum DeviceType {
     USBController,
     PCIeController,
     MainProcessor,
     DRAM,
     /// ROM, not mass storage (usb/pcie)
-    FlashMemory
+    FlashMemory,
+    Unknown,
 }
+
+#[repr(C)]
 pub struct PageTableTTBR1;
 
-pub struct ArcServices {
-    paging: PageTableTTBR1,
-    devices: &'static [ArcDevice],
-}
+#[repr(C)]
+pub struct MemoryRegion;
 
+#[repr(C)]
 pub struct ArcDevice {
     device_type: DeviceType,
     numa_id: usize,
 }
+
+impl ArcDevice {
+    pub fn new(device_type: DeviceType, numa_id: usize) -> Self {
+        Self {
+            device_type,
+            numa_id,
+        }
+    }
+}
+
+#[repr(C)]
+pub struct ArcServices<const D: usize, const M: usize> {
+    paging: PageTableTTBR1,
+    devices: [ArcDevice; D],
+    memory_map: [MemoryRegion; M],
+}
+
+impl<const D: usize, const M: usize> ArcServices<D, M> {
+    pub fn new(
+        paging: PageTableTTBR1,
+        devices: [ArcDevice; D],
+        memory_map: [MemoryRegion; M],
+    ) -> Self {
+        Self {
+            paging,
+            devices,
+            memory_map,
+        }
+    }
+}
+
+// DEFAULTS
+
+// for testing
+
+/// Should be called by arcboot to make the structures and passed to neutron entry
+pub fn make_default() {
+    let device = ArcDevice::new(DeviceType::DRAM, 0);
+    let devices = [device];
+    let memory_map = [MemoryRegion {}];
+    let service = ArcServices::new(PageTableTTBR1 {}, devices, memory_map);
+}
+
+// ---------------
+// TESTS
+// ---------------
 
 #[test]
 fn test_basics() {
--- src/bin/standard.rs
+++ src/bin/standard.rs
@@ -8,7 +8,10 @@
 // A standard entry uses the standard-<arch>.json target
 // And builds for a uboot-like target. With U-boot, have to also include a UBoot header
 
+#[no_mangle]
 extern "C" fn arcboot_entry() -> ! {
+    // make an ArcServices, then setup kernel in higher half, load its segments to tis proper positions (ELF)
+    // then `j arc_entry` or jump to that address. Its the virt addr isnt it?
     loop {}
 }
 
