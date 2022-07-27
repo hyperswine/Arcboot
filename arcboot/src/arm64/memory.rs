2de4609df579c0e215395a3966f0a1ba88495b50
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-07-27 10:20:26 +1000

    More

--- arcboot/src/arm64/memory.rs
+++ arcboot/src/arm64/memory.rs
@@ -1,7 +1,8 @@
+/// Setup virtual memory with MMIO/DMA. Uses DEVICE TREE or UEFI
+/// Make sure to make arcboot use TTBR0 addressing (all 0s), while the Kernel uses TTBR1 addressing
 //-------------------
 // IMPORT
 //-------------------
-
 use arcboot_api::MemoryMap;
 use bitfield::bitfield;
 use core::intrinsics::unlikely;
@@ -19,16 +20,6 @@ const PAGE_MASK: usize = PAGE_SIZE - 1;
 type PageTableIndex = [bool; 9];
 type PageTableOffset = [bool; 12];
 
-// -------------
-// HANDLERS
-// -------------
-
-/// In order to bookkeep page tables, require a pointer and a page fault handler. Or non existent page handler
-fn on_page_fault() {
-    // if page does not exist in the page table, create a mapping using the next free frame, or page is swapped to disk
-    let next_free_frame: usize;
-}
-
 // ------------------
 // DESCRIPTORS
 // ------------------
@@ -39,24 +30,25 @@ fn on_page_fault() {
 bitfield! {
     pub struct TableDescriptor4K(u64);
     impl Debug;
-    u32;
+    u64;
     pub ns_table, set_ns_table: 63;
     pub ap_table, set_ap_table: 62, 61;
     pub xn_table, set_xn_table: 60;
     pub pxn_table, set_pxn_table: 59;
     pub next_lvl_table_addr, set_next_lvl_table_addr: 47, 12;
-    pub ones, set_ones: 1, 0;
+    pub id_one, set_one: 1;
+    pub valid, set_valid :0;
 }
 
 // 4K => starts with 0's
 bitfield! {
     pub struct BlockDescriptor4K(u64);
     impl Debug;
-    u32;
+    u64;
     pub custom, set_custom: 58, 55;
     pub uxn, set_uxn: 54;
     pub pxn, set_pxn: 53;
-    pub contig, set_config: 52;
+    pub contig, set_contig: 52;
     pub output_addr, set_output_addr: 47, 12;
     pub zeroes, set_zeroes: 1, 0;
     pub non_gathering, set_non_gathering: 11;
@@ -65,13 +57,27 @@ bitfield! {
     pub access_permissions, set_access_permissions: 7, 6;
     pub non_secure, set_non_secure: 5;
     pub index_into_mair, set_index_into_mair: 4, 2;
+    pub id_zero, set_zero: 1;
+    pub valid, set_valid: 0;
+}
+
+pub fn default_unmapped_table_descriptor() -> TableDescriptor4K {
+    let res = TableDescriptor4K(0);
+
+    res
+}
+
+pub fn default_unmapped_block_descriptor() -> BlockDescriptor4K {
+    let res = BlockDescriptor4K(0);
+
+    res
 }
 
 // 4K VAddr (should be 1s for TTBR1)
 bitfield! {
     pub struct VAddr48_4K(u64);
     impl Debug;
-    u16;
+    u64;
     pub sign, set_sign: 63, 48;
     pub l0_index, set_l0_index: 47, 39;
     pub l1_index, set_l1_index: 38, 30;
@@ -193,10 +199,6 @@ pub unsafe fn enable_mmu_and_caching(phys_tables_base_addr: u64) -> Result<(), &
     Ok(())
 }
 
-// Where to setup page tables? Maybe at 0x4000_1000 DRAM? Really anywhere that isnt used rn, esp by the bootloader. If load bootloader at 0x80_000 DRAM and heap 0x4000_0000-0x4100_0000, start page tables TTBR1 at 0x4100_0000
-// Kernel should setup TTBR0 tables if possible. Though prob already setup by uboot. If ID mapped TTBR0, just leave it (since arcboot uses TTBR0 for its own code)
-// * Make sure to make arcboot use TTBR0 addressing (all 0s)
-
 pub fn setup() {
     unsafe {
         let res = enable_mmu_and_caching(0x4000_1000);
@@ -207,12 +209,10 @@ pub fn setup() {
     }
 }
 
-pub fn goto_table_descriptor(addr: u64) -> u64 {
-    // dont get MMU to walk the table, manually do it
-    let x: u64 = 1;
-    // cast x as STAGE 1 descriptor
-
-    0
+/// Get the value of TTBR1
+#[inline(always)]
+pub fn ttbr1() -> u64 {
+    TTBR1_EL1.get()
 }
 
 pub const KERNEL_BOOT_STACK_PAGES: usize = 16;
@@ -220,26 +220,125 @@ pub const KERNEL_BOOT_STACK_START: u64 = 0xFFFF_FFFF_FFFF_FFFF;
 pub const KERNEL_BOOT_HEAP_START: u64 = 0x0000_FFFF_FFFF_FFFF;
 pub const KERNEL_BOOT_HEAP_PAGES: usize = 16;
 
-// MMIO and other memory regions. USE DEVICE TREE or UEFI
+pub fn get_table_desc(base_addr: u64, index: u64) -> u64 {
+    unsafe {
+        // maybe encapsulate this logic into walk_table(base_addr, index) -> u64
+        let level_descriptor_addr = (base_addr + index * 8) as *const u64;
+        let level_descriptor = core::ptr::read_volatile(level_descriptor_addr);
 
-// ? Get a few free frames that you know ought to be free
-// NOT POSSIBLE AS A STATIC METHOD, only for neutron when you link
-// pub fn boot_free_frames() -> &'static [u64] {
-//     static res: [u64; 1] = [0; 1];
+        level_descriptor
+    }
+}
 
-//     &res
-// }
+/// For L0-L2 Page Tables
+pub fn get_next_lvl_table(base_pt_addr: u64, l_index: u64) -> Option<u64> {
+    let level_descriptor = TableDescriptor4K(get_table_desc(base_pt_addr, l_index));
 
-pub fn set_frame_used(frame: u64) {}
+    // if valid, return Some & decode descriptor's addr that it points to
+    if level_descriptor.valid() {
+        Some(level_descriptor.next_lvl_table_addr())
+    } else {
+        None
+    }
+}
+
+/// For L3 Page Table
+pub fn get_output_addr_block(base_pt_addr: u64, l_index: u64) -> Option<u64> {
+    let level_descriptor = BlockDescriptor4K(get_table_desc(base_pt_addr, l_index));
+
+    if level_descriptor.valid() {
+        Some(level_descriptor.output_addr())
+    } else {
+        None
+    }
+}
+
+/// If there is already a table there, it will overwrite it. So make sure you dont do something dumb
+/// Returns the PAddr of the new table start (NOT NEEDED, but oh well)
+pub fn make_page_table(free_frame: u64) -> u64 {
+    // create an l1 table and update that descriptor
+    let frame_addr = (free_frame * PAGE_SIZE as u64) as *mut u64;
+    pub const N_PAGES_IN_TABLE: u64 = 512;
+
+    // start writing to it in unsafe
+    unsafe {
+        // for each page (0..2^9), set all entries to default table descriptors that are invalid
+        for page_number in 0..2_u64.pow(9) {
+            let unmapped_desc = default_unmapped_table_descriptor();
+            core::ptr::write_volatile(frame_addr, unmapped_desc.0);
+        }
+    }
+
+    free_frame
+}
 
 /// Given a virtual address range, find free frames to map them to (4K aligned). Region_size: number of pages
 pub fn map_region_ttbr1<const N: usize>(
     region_start: u64,
-    region_size: usize,
+    n_pages: u64,
     free_frames: &mut FreePages<N>,
 ) {
-    for r in 0..region_size {
-        // ttbr1.walk_addr_entry
+    // we already calc number of pages (ceiling) required to map the vaddr range
+
+    for page in 0..n_pages {
+        // attempt to map a frame (should prob return Option?)
+        let frame_number = free_frames.pop();
+
+        // this is the vaddr. NOTE: should have 16 1's from MSB
+        let mut vaddr_start = region_start + page * PAGE_SIZE as u64;
+        // ensure addr will be a TTBR1 addr
+        vaddr_start = vaddr_start | 0xFFFF_0000_0000_0000;
+
+        let actual_vaddr = VAddr48_4K(vaddr_start);
+
+        // walk the table to map each index
+        let base_pt_addr = ttbr1();
+
+        // l0 index (47..39)
+
+        // IF ALREADY MAPPED, continue; or maybe panic!? or maybe overwrite?
+        // or maybe send an enum Continue, Panic, Overwrite
+        // FOR NOW: if one of the levels returns Option::None, make the descriptor using another free frame
+
+        let l0_index = actual_vaddr.l0_index();
+        let l1_table_addr = get_next_lvl_table(base_pt_addr, l0_index);
+
+        let l2_table_addr = match l1_table_addr {
+            Some(l) => get_next_lvl_table(base_pt_addr, l),
+            None => {
+                // make this into a gen_new_table(prev_table_base_addr, TABLE/BLOCK) function, and maybe return none as well
+
+                // create an l1 table and update that descriptor
+                let table_frame = free_frames.pop();
+                // POINT THE ORIGINAL L0 ENTRY TO NEW as well. Pass L0 entry u64 addr to it?
+                // Point and set to valid basically, for entry curr-1
+                make_page_table(table_frame);
+                let mut l0_desc = TableDescriptor4K(get_table_desc(base_pt_addr, l0_index));
+                // set valid and next_level_table_flags
+                l0_desc.set_valid(true);
+                l0_desc.set_next_lvl_table_addr(table_frame);
+
+                // write volatile to point at new table addr
+                unsafe {
+                    core::ptr::write_volatile(table_frame as *mut u64, l0_desc.0);
+                }
+
+                // return the next one, which is unmapped
+                None
+            }
+        };
+
+        let l3_table_addr = match l2_table_addr {
+            Some(l) => get_next_lvl_table(base_pt_addr, l),
+            None => {
+                let table_frame = free_frames.pop();
+                make_page_table(table_frame);
+                None
+            }
+        };
+
+        // NOW, get the output addr (since L3 entries are block descriptors)
+        let output_addr = get_output_addr_block(base_pt_addr, l3_table_addr);
     }
 }
 
@@ -254,13 +353,25 @@ pub fn setup_kernel_tables(memory_map: MemoryMap) {
 
     // map 16 pages from high
     map_region_ttbr1(
-        KERNEL_BOOT_STACK_START - 16 * 4096,
-        KERNEL_BOOT_STACK_PAGES,
+        KERNEL_BOOT_STACK_START - 16 * PAGE_SIZE as u64,
+        KERNEL_BOOT_STACK_PAGES as u64,
         &mut free_frames,
     );
     map_region_ttbr1(
         KERNEL_BOOT_HEAP_START,
-        KERNEL_BOOT_HEAP_PAGES,
+        KERNEL_BOOT_HEAP_PAGES as u64,
         &mut free_frames,
     );
 }
+
+// -------------
+// HANDLERS
+// -------------
+
+/// In order to bookkeep page tables, require a pointer and a page fault handler. Or non existent page handler
+fn on_page_fault() {
+    // if page does not exist in the page table, create a mapping using the next free frame, or page is swapped to disk
+    let next_free_frame: usize;
+}
+
+// TTBR0 (Arcboot Addressing)
